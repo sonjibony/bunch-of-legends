cc9089b35164cb65b8d2a7b48b2f44379a87b15c
sonjibony <sonjibony@gmail.com>
2022-08-22 18:26:59 +0600

    button edited

--- calculation.js
+++ calculation.js
@@ -1,38 +1,38 @@
 /*-------------------
 function for bonus
 --------------------*/
-function elementValue(elementId){
-const element = document.getElementById(elementId);
-const elementValue=parseFloat(element.value);
-return elementValue;
+function elementValue(elementId) {
+  const element = document.getElementById(elementId);
+  const elementValue = parseFloat(element.value);
+  return elementValue;
 }
 
 /*-------------------------
 calculating player expenses
 ---------------------------*/
-document.getElementById('btn-calculate').addEventListener('click',function(){
-   const selectedTotalElement = document.getElementById('selected');
-    const selectedTotal = selectedTotalElement.innerText;
-    const budgetFieldElement = document.getElementById('budget-field');
-    //budget calculation
-    const budgetField = (budgetFieldElement.value)*selectedTotal;
-    const expensesFieldElement = document.getElementById('expenses-field');
-    expensesFieldElement.innerText = budgetField;
-    
-})
+document.getElementById("btn-calculate").addEventListener("click", function () {
+  const selectedTotalElement = document.getElementById("selected");
+  const selectedTotal = selectedTotalElement.innerText;
+  const budgetFieldElement = document.getElementById("budget-field");
+  //budget calculation
+  const budgetField = budgetFieldElement.value * selectedTotal;
+  const expensesFieldElement = document.getElementById("expenses-field");
+  expensesFieldElement.innerText = budgetField;
+});
 
 /*--------------------------
    calculating total cost
 --------------------------*/
-document.getElementById('btn-calculate-total').addEventListener('click',function(){
-    
-    const expensesFieldElement = document.getElementById('expenses-field');
+document
+  .getElementById("btn-calculate-total")
+  .addEventListener("click", function () {
+    const expensesFieldElement = document.getElementById("expenses-field");
     const expensesField = expensesFieldElement.innerText;
-   // using bonus function 
-  const coachCost= elementValue('coach-field');
-  const managerCost= elementValue('manager-field');
-// total cost calculation
-  const totalFieldElement = document.getElementById('total-field');
-    totalFieldElement.innerText = parseFloat (expensesField)+managerCost+coachCost;
-  
-})
+    // using bonus function
+    const coachCost = elementValue("coach-field");
+    const managerCost = elementValue("manager-field");
+    // total cost calculation
+    const totalFieldElement = document.getElementById("total-field");
+    totalFieldElement.innerText =
+      parseFloat(expensesField) + managerCost + coachCost;
+  });
--- selection.js
+++ selection.js
@@ -3,53 +3,51 @@ player selection section
 ------------------------*/
 const playerArray = [];
 
-function selectedPlayers(playerArray){
-   const selectedElement = document.getElementById('selected');
-    const player = document.getElementById('selected-players')
-    player.innerText='';
-// looping to access array elements
-        for(let i= 0; i<playerArray.length; i++){
-            let  players = playerArray[i]; 
+function selectedPlayers(playerArray) {
+  const selectedElement = document.getElementById("selected");
+  const player = document.getElementById("selected-players");
+  player.innerText = "";
+  // looping to access array elements
+  for (let i = 0; i < playerArray.length; i++) {
+    let players = playerArray[i];
     // appending order list
-        let ol = document.createElement('ol');
+    let ol = document.createElement("ol");
     ol.innerHTML = `
-    <li class="list-group-item fw-bold">${i+1}. ${players}</li> 
-`
-player.appendChild(ol);
-selectedElement.innerText = playerArray.length;
-console.log(playerArray.length,playerArray[i])
-}
+    <li class="list-group-item fw-bold">${i + 1}. ${players}</li> 
+`;
+    player.appendChild(ol);
+    selectedElement.innerText = playerArray.length;
+    console.log(playerArray.length, playerArray[i]);
+  }
 }
 
 /*----------------------------
  selected players list created
 -----------------------------*/
-function addToCart (element){
-   const playerName =  element.parentNode.children[0].innerText;
-  playerArray.push(playerName)
- // error message condition for more than five players
-if(playerArray.length<6){
-  selectedPlayers(playerArray)
-}
-else{
-    alert('Sorry you cant choose more than five players');
+function addToCart(element) {
+  const playerName = element.parentNode.children[0].innerText;
+  playerArray.push(playerName);
+  // error message condition for more than five players
+  if (playerArray.length < 6) {
+    selectedPlayers(playerArray);
+  } else {
+    alert("Sorry you cant choose more than five players");
     return;
+  }
 }
- }
 
 /*-------------------------------------------
  made buttons disabled after clicked for BONUS
 -------------------------------------------*/
-const buttons = document.querySelectorAll(".btn-disabled")
- for(let button of buttons){
-    button.addEventListener('click',function(event){
-        let button =event.target;
-        if(playerArray.length<6){
-        button.setAttribute('disabled',true);
-        button.style.backgroundColor = "black";
-        }
-        else{
-            return;
-        }
-    })
- }
+const buttons = document.querySelectorAll(".btn-disabled");
+for (let button of buttons) {
+  button.addEventListener("click", function (event) {
+    let button = event.target;
+    if (playerArray.length < 6) {
+      button.setAttribute("disabled", true);
+      button.style.backgroundColor = "black";
+    } else {
+      return;
+    }
+  });
+}
