916cd40e6011a3b1aec518bccd2b034e955af13b
sonjibony <sonjibony@gmail.com>
2022-08-22 19:15:08 +0600

    error fixed

--- calculation.js
+++ calculation.js
@@ -2,8 +2,16 @@
 function for bonus
 --------------------*/
 function elementValue(elementId) {
+
   const element = document.getElementById(elementId);
+    //error handling
+if(Math.sign(element.value)===-1){
+    alert('Please provide valid input');
+    element.value= '';
+    return;
+  }
   const elementValue = parseFloat(element.value);
+  
   return elementValue;
 }
 
@@ -14,6 +22,13 @@ document.getElementById("btn-calculate").addEventListener("click", function () {
   const selectedTotalElement = document.getElementById("selected");
   const selectedTotal = selectedTotalElement.innerText;
   const budgetFieldElement = document.getElementById("budget-field");
+  //error handling
+  if(Math.sign(budgetFieldElement.value)===-1){
+    alert('Please provide valid input');
+    budgetFieldElement.value ='';
+    return;
+  }
+
   //budget calculation
   const budgetField = budgetFieldElement.value * selectedTotal;
   const expensesFieldElement = document.getElementById("expenses-field");
@@ -31,8 +46,14 @@ document
     // using bonus function
     const coachCost = elementValue("coach-field");
     const managerCost = elementValue("manager-field");
+    
     // total cost calculation
+    
     const totalFieldElement = document.getElementById("total-field");
+
+    
+    
     totalFieldElement.innerText =
       parseFloat(expensesField) + managerCost + coachCost;
+    console.log(totalFieldElement.innerText);
   });
