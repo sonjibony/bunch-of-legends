304944d57f9a88a2ac9845a0e2e6389bd203b4e1
sonjibony <sonjibony@gmail.com>
2022-08-22 17:50:44 +0600

    total calculation resolved

--- calculation.js
+++ calculation.js
@@ -13,28 +13,26 @@ calculating player expenses
 document.getElementById('btn-calculate').addEventListener('click',function(){
    const selectedTotalElement = document.getElementById('selected');
     const selectedTotal = selectedTotalElement.innerText;
-    
-  
-const budgetFieldElement = document.getElementById('budget-field')
+    const budgetFieldElement = document.getElementById('budget-field');
+    //budget calculation
     const budgetField = (budgetFieldElement.value)*selectedTotal;
     const expensesFieldElement = document.getElementById('expenses-field');
     expensesFieldElement.innerText = budgetField;
     
-    //console.log(budgetField,coachCost,managerCost)
 })
 
 /*--------------------------
    calculating total cost
 --------------------------*/
-
 document.getElementById('btn-calculate-total').addEventListener('click',function(){
     
     const expensesFieldElement = document.getElementById('expenses-field');
-    const expensesField = expensesFieldElement.innerText
+    const expensesField = expensesFieldElement.innerText;
+   // using bonus function 
   const coachCost= elementValue('coach-field');
   const managerCost= elementValue('manager-field');
-
+// total cost calculation
   const totalFieldElement = document.getElementById('total-field');
     totalFieldElement.innerText = parseFloat (expensesField)+managerCost+coachCost;
-  //console.log(coachCost,managerCost)
+  
 })
